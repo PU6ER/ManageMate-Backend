89e22876658ccc63f099e7b6f5905f3bc06365ba
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-01-06 20:46:03 +0300

    feat: add auth constants

---
+++ src/auth/constants.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const jwtConstants = {
+  secret: 'ILOVENEST',
+};
