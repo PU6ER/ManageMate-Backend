c8a9af767ecff3bab2ac399b605fae0158c61698
PU6ER <djakobstv@gmail.com>
2024-03-04 08:58:57 +0300

    feat: update providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
-import { SubtaskModule } from './subtask/subtask.module';
-import { ProjectModule } from './project/project.module';
+
 import { UserModule } from './user/user.module';
+
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
+import { TimeBlockModule } from './time-block/time-block.module'
+import { PomodoroModule } from './pomodoro/pomodoro.module'
 
 @Module({
-  imports: [TaskModule, SubtaskModule, ProjectModule, UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, TaskModule, TimeBlockModule, PomodoroModule],
 })
 export class AppModule {}
